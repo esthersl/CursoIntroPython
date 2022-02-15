5c85f66698cf5a41ce38241efc8e29fbe4c1a8a0
esthersl <91384775+esthersl@users.noreply.github.com>
2022-02-14 23:33:02 -0600

    Add files via upload
    Espero haberlo hecho bien

---
+++ ejercc1.py
@@ -0,0 +1,28 @@
+sum = 1 + 2
+print(sum)
+# Impresión en pantalla print('Hola desde la consola')#
+print("Hola desde la consola")
+
+#VARIABLES#
+product = sum * 2
+print(product)
+# Tu turno, prueba el fragmento de código anterior
+from datetime import date
+date.today()
+print(date.today())
+print("HOY ES : " , date.today())
+
+print("Bienvenido al programa de bienvenida")
+name = input("Introduzca su nombre ")
+print("Saludos: " + name)
+
+print("Calculadora")
+first_number = int(input("Primer número: "))
+second_number = int(input("Segundo número: "))
+num = first_number + second_number
+print(first_number , second_number)
+sm = sum + sum
+print(sm)
+print(num)
+
+
